33bb7e63aca067053f558bcf02a04ab8828e3fd8
CaoJamie <me@caojamie.io>
2025-02-05 16:57:17 +0000

    update main thread

--- src/baseline/main.py
+++ src/baseline/main.py
@@ -30,63 +30,59 @@ Use this step-by-step format:
 
 Regardless of the approach, always output only code in correct format.
 Do not include any explination, only include them in the necessary comments starting with `#`.
-
 """
 
-
-
-
-
 logging.basicConfig(level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument("--dataset", type=str, default="openai/openai_humaneval")
-argparser.add_argument("--output", type=str, default="humaneval_results.json")
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--dataset", type=str, default="openai/openai_humaneval")
+    argparser.add_argument("--output", type=str, default="humaneval_results.json")
+    args = argparser.parse_args()
 
-args = argparser.parse_args()
+    # initalize config
+    config = Config()
+    logger.log(logging.INFO, "Loaded Config: %s", config)
 
-# initalzie config
-config = Config()
-logger.log(logging.INFO, "Loaded Config: %s", config)
+    num_gpus = torch.cuda.device_count()
 
-
-num_gpus = torch.cuda.device_count()
-
-
-prm = load_prm(config)
+    prm = load_prm(config)
     
-# initalize vllm model
-llm = LLM(
-    model=config.model_path,
-    gpu_memory_utilization=config.gpu_memory_utilization,
-    enable_prefix_caching=True,
-    seed=config.seed,
-    tensor_parallel_size=num_gpus,
+    # initalize vllm model
+    llm = LLM(
+        model=config.model_path,
+        gpu_memory_utilization=config.gpu_memory_utilization,
+        enable_prefix_caching=True,
+        seed=config.seed,
+        tensor_parallel_size=num_gpus,
     )
 
-# initalize dataset
-if args.dataset:
-    if args.dataset == "openai/openai_humaneval":
-        system_prompt = HUMAN_EVAL_PROMPT
-        dataset = load_dataset("openai/openai_humaneval")
-        data = dataset["test"]
-        examples = {}
-        # initalize question
-        examples["problem"] = [system_prompt + prompt for prompt in data["prompt"]]
-        examples["task_id"] = data["task_id"]
+    # initalize dataset
+    if args.dataset:
+        if args.dataset == "openai/openai_humaneval":
+            system_prompt = HUMAN_EVAL_PROMPT
+            dataset = load_dataset("openai/openai_humaneval")
+            data = dataset["test"]
+            examples = {}
+            # initalize question
+            examples["problem"] = [system_prompt + prompt for prompt in data["prompt"]]
+            examples["task_id"] = data["task_id"]
 
-# dumpt the example object
-with open("example.json", "w") as f:
-    json.dump(examples, f)
+    # dumpt the example object
+    with open("example.json", "w") as f:
+        json.dump(examples, f)
 
-logger.log(logging.INFO, "Starting beam search")
-beam_search_results = beam_search(examples, llm=llm, prm=prm, config=config)
+    logger.log(logging.INFO, "Starting beam search")
+    beam_search_results = beam_search(examples, llm=llm, prm=prm, config=config)
 
-logger.log(logging.INFO, "Storing result")
-# store results
-with open(args.output, "w") as f:
-    json.dump(beam_search_results, f)
+    logger.log(logging.INFO, "Storing result")
+    # store results
+    with open(args.output, "w") as f:
+        json.dump(beam_search_results, f)
+
+
+if __name__ == "__main__":
+    main()
